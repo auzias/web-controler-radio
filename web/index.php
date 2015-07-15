78153bcfc456cbc20d741339fdca82ae1702c46e
auzias <github@auzias.net>
2015-07-15 13:14:36 +0200

    :file_folder: test.html file renamed into debug.html. php output if debug = ON

--- web/index.php
+++ web/index.php
@@ -1,5 +1,10 @@
 <?php
-  include ('mock-up/ihm.php');
+  if (isset($_GET[debug])) { //Debug mode
+    include ('mock-up/debug.html');
+  } else {
+    include ('mock-up/ihm.html');
+  }
+
   $output = "";
   if (isset($_POST[action])) { //If an action is requested and the variable is set:
 
@@ -7,14 +12,12 @@
     if ($_POST["action"] == "mute") {
       echo '<pre>muting</pre>';
       $output = shell_exec('sudo amixer set Master mute');
-      echo "<pre>$output</pre>";
     }
 
     //Unmute the sounds system:
     if ($_POST["action"] == "unmute") {
       echo '<pre>unmuting</pre>';
       $output = shell_exec('sudo amixer set Master unmute');
-      echo "<pre>$output</pre>";
     }
 
     //Set the volume level:
@@ -23,16 +26,13 @@
         $level = $_POST["level"];
         if ($level >= 0 && $level <= 100) { //Is the $level value correct?
           $output = shell_exec('sudo amixer set Master '.$level.'%');
-          echo "<pre>$output</pre>";
         }
       }
     }
-  $output = shell_exec('whoami');
-  echo "<pre>$output</pre>";
-  } else { //end (isset($_POST["action"]))
-    echo '<pre>no $_POST["action"] has been set.</pre>';
   }
 
-  echo "<pre>$output</pre>";
+  if (isset($_GET[debug])) { //Debug mode
+    echo "<pre>$output</pre>";
+  }
 
 ?>
