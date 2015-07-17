9d4edb01b13def9c0f37aaeb6c0b2ca4f84bcca7
auzias <github@auzias.net>
2015-07-17 16:25:48 +0200

    :sound: pages inclusion cleared and corrected

--- web/php/pages.php
+++ web/php/pages.php
@@ -2,12 +2,10 @@
 
   if (isset ($_GET[debug])) { //Debug mode
     include ('mock-up/debug.html');
+  } else if (isset ($_GET[level])) { //Debug mode
+    echo get_volume ();
   } else {
     include ('mock-up/ihm.html');
-  }
-
-  if (isset ($_GET[level])) { //Debug mode
-    echo get_volume ();
   }
 
 ?>
