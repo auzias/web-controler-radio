0d146fb372b26a5215e69788a08a41a1d5533111
auzias <github@auzias.net>
2015-07-17 14:09:53 +0200

    :sound: action-volume.php: variable usage for a clearer code

--- web/php/action-volume.php
+++ web/php/action-volume.php
@@ -1,19 +1,25 @@
 <?php
+$VOL_MUTE = "mute";
+$VOL_UNMUTE = "unmute";
+$VOL_SET = "set";
+$VOL_UP = "up";
+$VOL_DOWN = "down";
+$action = $_POST["volume"];
 
   //Mute the sound sytem:
-  if ($_POST["volume"] == "mute") {
+  if ($action == $VOL_MUTE) {
     dprint('muting');
     /* while `#amixer set Master mute` work fine,
        `#amixer set Master unmute` does not work at all.
        A tweak is then needed, that is to save the sound level in a cookie,
        before muting, and reading this same cookie to unmute.
-    */
+     */
     set_cookie_current_volume();
     $output = set_volume (0);
   }
 
   //Unmute the sounds system:
-  if ($_POST["volume"] == "unmute") {
+  if ($action == $VOL_UNMUTE) {
     if (get_volume() == 0) {
       dprint ('unmuting');
       set_volume ($_COOKIE['level']);
@@ -23,21 +29,21 @@
   }
 
   //Set the volume level:
-  if ($_POST["volume"] == "volume") {
+  if ($action == $VOL_SET) {
     if (isset ($_POST["level"])) { //Is the level variable also set?
-      set_volume ($_POST["level"]);
       dprint ('set '.$_POST["level"]);
+      set_volume ($_POST["level"]);
     }
   }
 
   //Set the sound level 5% louder:
-  if ($_POST["volume"] == "up") {
+  if ($action == $VOL_UP) {
     dprint('+5%');
     $output = shell_exec('sudo amixer set Master 5%+');;
   }
 
   //Set the sound level 5% lower:
-  if ($_POST["volume"] == "down") {
+  if ($action == $VOL_DOWN) {
     dprint('+5%');
     $output = shell_exec('sudo amixer set Master 5%-');;
   }
