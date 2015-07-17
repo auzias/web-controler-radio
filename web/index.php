9a1d559a4f4ffa6c16a12b438441625104f03524
auzias <github@auzias.net>
2015-07-17 14:15:19 +0200

    :sound: debug details removed

--- web/index.php
+++ web/index.php
@@ -1,17 +1,13 @@
 <?php
-  require_once("./php/functions.php");
-
   $output = "";
   $DEFAULT_LEVEL = 30;
 
+  require_once("./php/functions.php");
   require_once("./php/pages.php");
   require_once("./php/actions.php");
 
   dprint ("saved level:".$_COOKIE['level']);
   $output = shell_exec("sudo amixer get Master");
   echo "<pre>$output</pre>";
-  echo "<br><br><br>";
 
-  $output = shell_exec("sudo amixer get Master | grep 'Mono:' | awk '{print $4}'| tr -d '[%]'");
-  echo "<pre>".$output."</pre>";
 ?>
