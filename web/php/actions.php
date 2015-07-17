16301002982659f524b677d536fa8ee0df511a30
auzias <github@auzias.net>
2015-07-17 15:06:07 +0200

    :sound: typo

--- web/php/actions.php
+++ web/php/actions.php
@@ -1,11 +1,9 @@
 <?php
 
-
   if (isset ($_POST["volume"])) { //If an action on the volume is requested:
       include("./php/action-volume.php");
   } else {
-      drpint("POST volume not set");
+      dprint("POST volume not set");
   }//end of isset ($_POST["volume"])
-
 
 ?>
